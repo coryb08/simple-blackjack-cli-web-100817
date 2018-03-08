7dd641a81d90edc8927cb2c69cfd523af622f846
github username <github email address>
2018-03-08 18:03:41 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,57 +1,51 @@
+
 def welcome
-  puts "Welcome to the Blackjack Table";
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  return rand(2..10);
+  card = rand(1..11)
 end
 
-def display_card_total(cardtotal)
-  puts "Your cards add up to #{cardtotal}";
-  # hit?(card_total);
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
+
 end
 
 def prompt_user
-  puts "Type 'h' to hit or 's' to stay";
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  return gets.split.chomp;
+  return gets.chomp
 end
 
-def end_game(cardtotal)
-  puts "Sorry, you hit #{cardtotal}. Thanks for playing!";
+def end_game(num)
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
-  total = deal_card + deal_card;
-  display_card_total(total);
-  # hit?(total);
-  return total
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
 
-def hit?(num)
-  prompt_user;
-  newCard = deal_card;
-
-  newtotal = num + newCard;
-  answer = get_user_input;
-  if (answer != 'h') && (answer != 's')
-    invalid_command;
-    prompt_user;
-  elsif (answer === 'h') && (newtotal > 21)
-      return end_game(newtotal);
-  elsif (answer === 'h') && (newtotal <= 21)
-      display_card_total(newtotal);
-      card_total = newtotal
-      return
+def hit?(current_total)
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    current_total +=deal_card
+    return (current_total)
+  elsif input == "s"
+    return (current_total)
   else
-    return;
+    invalid_command(current_total)
   end
+
 end
 
-def invalid_command
-  puts "Please enter a valid command";
+def invalid_command(num)
+  hit?(num)
 end
 
 #####################################################
@@ -59,81 +53,16 @@ end
 #####################################################
 
 def runner
-  welcome;
-  initial_round
-  card_total = initial_round
-  while card_total <= 21
-    hit?(card_total)
+  welcome
+  num = initial_round
+  if num <= 21
+    until num > 21
+      new_num = hit?(num)
+      num = new_num
+      display_card_total(num)
+
+    end
   end
+  end_game(num)
 
 end
-
-
-#
-#
-# def deal_card
-#   # code #deal_card here
-#     rand(2..10)
-# end
-#
-# def display_card_total(card_total)
-#   # code #display_card_total here
-#   puts "Your cards add up to #{card_total}"
-#   return card_total
-# end
-#
-# def welcome
-#   # code #welcome here
-#   puts "Welcome to the Blackjack Table"
-# end
-#
-# def prompt_user
-#   # code #prompt_user here
-#   puts "Type 'h' to hit or 's' to stay"
-# end
-#
-# def initial_round
-#   # code #initial_round here
-#   sum = deal_card + deal_card
-#   display_card_total(sum)
-# end
-#
-# def end_game(card_total)
-#   # code #end_game here
-#     puts "Sorry, you hit #{card_total}. Thanks for playing!"
-# end
-#
-# def get_user_input
-#   # code #get_user_input here
-#   gets.strip.chomp
-# end
-#
-# def hit? (card_total)
-#   # code hit? here
-#   prompt_user
-#   if get_user_input == 'h'
-#     card_total += deal_card
-#   end
-#     card_total
-# end
-#
-# def invalid_command
-#   # code invalid_command here
-#   puts "Please enter a valid command"
-# end
-#
-# #####################################################
-# # get every test to pass before coding runner below #
-# #####################################################
-#
-# def runner
-#   # code runner here
-#   welcome
-#   card_total = initial_round
-#   while card_total < 21
-#     card_total = hit?(card_total)
-#     card_total = display_card_total(card_total)
-#   end
-#   end_game(card_total)
-#
-# end
